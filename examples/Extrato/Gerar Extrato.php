38928788866fb4f0b9dd4317052eed5c8af9cbab
Matheus Fidelis <msfidelis@outlook.com>
2017-08-09 17:24:09 -0300

    Extrato

--- examples/Extrato/Gerar Extrato.php
+++ examples/Extrato/Gerar Extrato.php
@@ -9,13 +9,29 @@ $chave = "ef947cf5867488f744b82744dd3a8fc4852e529f";
 
 $PJBankRecebimentos = new Recebimento($credencial, $chave);
 
-//Gera um novo extrato
+echo("Gerando o extrato somente com recebiments liquidados" . PHP_EOL);
+
 $extrato = $PJBankRecebimentos->Extratos->NovoExtrato();
 $extrato
     ->apenasPagos()
     ->gerar();
 
+
 print_r($extrato->getItens());
+
+echo("Gerando o extrato filtrado por data" . PHP_EOL);
+
+$extrato = $PJBankRecebimentos->Extratos->NovoExtrato();
+$extrato
+    ->setDataInicio("06/01/2017")
+    ->setDataFim("01/30/2017")
+    ->gerar();
+
+
+print_r($extrato->getItens());
+
+
+
 
 
 
